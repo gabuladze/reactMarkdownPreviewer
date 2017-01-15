31b58e1ccaabd8838b76eb4e954785270adcfee9
Giorgi Abuladze <g.abuladze@live.com>
2017-01-15 19:01:15 +0400

    Changed string & added comments

--- src/Container.js
+++ src/Container.js
@@ -9,16 +9,18 @@ class Container extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*'
+      value: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\n *[Giorgi Abuladze](https://freecodecamp.com/gabuladze)*'
     }
   }
 
+  // Update the state variable
   updateValue(event) {
     this.setState({
       value: event.target.value
     });
   }
 
+  // Parse the input & generate the output
   parseInput(input) {
     let content = marked(input, { sanitize: true });
     return { __html: content };
